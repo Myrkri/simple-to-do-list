303994b4cb8d912b6015193df2dd3770cf13a0e3
Myrkri <pertsevoi@yahoo.com>
2025-07-09 21:41:52 +0300

    moved TODO

--- src/main/java/com/prom/project/todolist/dto/ToDoDto.java
+++ src/main/java/com/prom/project/todolist/dto/ToDoDto.java
@@ -6,7 +6,6 @@ import java.time.LocalDate;
 
 @Data
 public class ToDoDto {
-    //TODO do I need this `id` field?
     private Integer id;
     private String description;
     private LocalDate due;
