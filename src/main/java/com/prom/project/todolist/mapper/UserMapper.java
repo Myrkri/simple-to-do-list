9379e4bb2d86d2b4fa58dab1a4c7efa7b3c40e92
Myrkri <pertsevoi@yahoo.com>
2025-07-12 02:56:42 +0300

    update user mapper

--- src/main/java/com/prom/project/todolist/mapper/UserMapper.java
+++ src/main/java/com/prom/project/todolist/mapper/UserMapper.java
@@ -2,14 +2,19 @@ package com.prom.project.todolist.mapper;
 
 import com.prom.project.todolist.dto.UserDto;
 import com.prom.project.todolist.entity.UserEntity;
+import org.mapstruct.Context;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
 
     UserDto toUserDto(UserEntity user);
+
     @Mapping(target = "id", ignore = true)
-    UserEntity toUserEntity(UserDto userDto);
+    @Mapping(target = "password",
+            expression = "java(passwordEncoder.encode(userDto.getPassword()))")
+    UserEntity toUserEntity(UserDto userDto, @Context PasswordEncoder passwordEncoder);
 
 }
