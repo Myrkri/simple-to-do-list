5552854dd86b210d8cc542962ee40f09320aa65d
Myrkri <pertsevoi@yahoo.com>
2025-07-12 02:15:22 +0300

    add search by username to validate user

--- src/main/java/com/prom/project/todolist/repository/UserRepository.java
+++ src/main/java/com/prom/project/todolist/repository/UserRepository.java
@@ -8,4 +8,5 @@ import org.springframework.stereotype.Repository;
 public interface UserRepository extends JpaRepository<UserEntity,Integer> {
 
     boolean existsByUsername(String username);
+    Optional<UserEntity> findByUsername(String username);
 }
