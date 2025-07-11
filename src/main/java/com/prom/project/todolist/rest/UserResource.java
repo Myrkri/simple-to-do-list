95c1a43b7258e73de16892beee88e2f5a41f790b
Myrkri <pertsevoi@yahoo.com>
2025-07-12 01:57:25 +0300

    connect User Resource and Service

--- src/main/java/com/prom/project/todolist/rest/UserResource.java
+++ src/main/java/com/prom/project/todolist/rest/UserResource.java
@@ -1,11 +1,11 @@
 package com.prom.project.todolist.rest;
 
+import com.prom.project.todolist.dto.UserDto;
 import com.prom.project.todolist.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -17,21 +17,19 @@ public class UserResource {
 
     @Operation(summary = "Api to login into the system")
     @PostMapping("/login")
-    public ResponseEntity<String> login(@RequestBody User user) {
-        //TODO return token
-        return null;
+    public ResponseEntity<String> login(@RequestBody UserDto user) {
+        return ResponseEntity.ok(userService.login(user));
     }
 
     @Operation(summary = "Api to register user into the system")
     @PostMapping("/signup")
-    public ResponseEntity<String> signup(@RequestBody User user) {
-        //TODO return token
-        return null;
+    public ResponseEntity<String> signup(@RequestBody UserDto user) {
+        return ResponseEntity.ok(userService.register(user));
     }
 
     @Operation(summary = "Api to get currently logged in user data")
     @GetMapping("/me")
-    public ResponseEntity<User> currentUser() {
-        return null;
+    public ResponseEntity<UserDto> currentUser() {
+        return ResponseEntity.ok(userService.getCurrentUser());
     }
 }
--- src/main/java/com/prom/project/todolist/service/UserService.java
+++ src/main/java/com/prom/project/todolist/service/UserService.java
@@ -1,4 +1,10 @@
 package com.prom.project.todolist.service;
 
+import com.prom.project.todolist.dto.UserDto;
+
 public interface UserService {
+
+    String login(UserDto user);
+    String register(UserDto user);
+    UserDto getCurrentUser();
 }
