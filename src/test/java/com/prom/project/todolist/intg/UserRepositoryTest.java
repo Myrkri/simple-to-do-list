7f57e63de14e6a09a354285028ea757d2be783a2
Myrkri <pertsevoi@yahoo.com>
2025-07-16 02:31:11 +0300

    testcontainers and UserRepo integration test

---
+++ src/test/java/com/prom/project/todolist/intg/UserRepositoryTest.java
@@ -0,0 +1,32 @@
+package com.prom.project.todolist.intg;
+
+import com.prom.project.todolist.entity.UserEntity;
+import com.prom.project.todolist.repository.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@DataJpaTest
+@ActiveProfiles("test")
+class UserRepositoryTest {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    void existsByUsername_returnsTrue_whenUserPresent() {
+        final UserEntity user = new UserEntity();
+        user.setUsername("test");
+        user.setPassword("password");
+        user.setRole("ROLE_USER");
+        userRepository.save(user);
+
+        boolean exists = userRepository.existsByUsername("test");
+
+        assertTrue(exists);
+    }
+}
+
