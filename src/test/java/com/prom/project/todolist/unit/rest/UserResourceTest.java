52646fcb03251bf3f568c1a3b50744f3c60fc848
Myrkri <pertsevoi@yahoo.com>
2025-07-15 23:40:46 +0300

    removed unneeded mock

--- src/test/java/com/prom/project/todolist/unit/rest/UserResourceTest.java
+++ src/test/java/com/prom/project/todolist/unit/rest/UserResourceTest.java
@@ -9,7 +9,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.context.annotation.Import;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
@@ -29,8 +28,6 @@ public class UserResourceTest {
     private MockMvc mvc;
     @MockitoBean
     private UserService userService;
-    @MockitoBean
-    private UserDetailsService userDetailsService;
     @MockitoBean
     private JwtDecoder jwtDecoder;
 
